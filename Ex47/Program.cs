a7c7049a8c9f24401ff706ba750d88685ce55d0c
Nattick <natalia.a.konovalova@gmail.com>
2023-03-07 11:53:47 +0300

    Ex47 actually works

--- Ex47/Program.cs
+++ Ex47/Program.cs
@@ -1,6 +1,39 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+
+int SizeOfMatrix(string text)
+{
+    Console.WriteLine(text);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void FillMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int num = new Random().Next(-100, 100);
+            matrix[i,j] = new Random().NextDouble() + num;
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        Console.WriteLine("\n");
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write($"{matrix[rows, columns]} | ");
+        }
+    }
+}
+
+double[,] matrix = new double[SizeOfMatrix("Please enter number of rows:"),
+                                SizeOfMatrix("Please enter number of columns:")];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
 
